cd80fcbd4e9bbb3e41cabdaae83733f7a9dd5850
a.vernay <alexisvernay38@gmail.com>
2018-03-20 11:06:24 +0100

    Création projet, gitignore, rajout de l'affichage

---
+++ src/main/java/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -0,0 +1,16 @@
+package fr.pizzeria.console;
+
+public class PizzeriaAdminConsoleApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("***** Pizzeria Administration ***** \n"
+				+ "1. Lister les pizzas \n"
+				+ "2. Ajouter une nouvelle pizza \n"
+				+ "3. Mettre à jour une pizza \n"
+				+ "4. Supprimer une pizza \n"
+				+ "99. Sortir \n");
+
+	}
+
+}
